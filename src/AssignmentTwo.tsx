db5a7217ef1d05170244cb34ce7a018bdb66bb6a
piyushpatilltf <156314123+piyushpatilltf@users.noreply.github.com>
2024-04-08 09:28:30 +0530

    modified

--- src/AssignmentTwo.tsx
+++ src/AssignmentTwo.tsx
@@ -50,8 +50,54 @@ function AssignmentTwo() {
   };
   const handleFirstPartyTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFirstPartyType(event.target.value.trim());
+  };
+  const HTMLComponent: React.FC<HTMLComponentProps> = ({ htmlString }) => {
+    const parseHTML = (html: string): ReactNode[] => {
+      const tempDiv = document.createElement('div');
+      tempDiv.innerHTML = html;
+      return Array.from(tempDiv.childNodes).map((node, index) => {
+        return createReactElement(node as HTMLElement, index);
+      });
+    };
+  
+    const createReactElement = (node: Node, index: number): ReactNode | null => {
+      if (node.nodeType === Node.TEXT_NODE) {
+        return (node as Text).textContent;
+      } else if (node.nodeType === Node.ELEMENT_NODE) {
+        const element = node as HTMLElement;
+        const tagName = element.tagName.toLowerCase();
+        const props: { [key: string]: string } = {};
+        for (let i = 0; i < element.attributes.length; i++) {
+          const { name, value } = element.attributes[i];
+          props[name] = value;
+        }
+  
+        const children = Array.from(element.childNodes).map((child, idx) => createReactElement(child, idx));
+        if(tagName==='span' && props['data-dpfor']){
+          switch (props['data-dpfor']) {
+            case "first_party_name":
+              return React.createElement(tagName, { key: index, ...props },firstPartyName);
+ 
+            case "first_party_state_of_organization":
+              return React.createElement(tagName, { key: index, ...props },firstPartyState);
+
+            case "first_party_type":
+              return React.createElement(tagName, { key: index, ...props },firstPartyType);
+            
+            default:
+              break;
+          }
+          
+        }
+        return React.createElement(tagName, { key: index, ...props }, ...children);
+      } else {
+        return null;
+      }
+    };
+  
+    return <div>{parseHTML(htmlString)}</div>;
   }; 
-  const renderData: string = `This Confidentiality Agreement (“<b>Agreement</b>”), by and between <span data-dpFor="first_party_name">${firstPartyName}</span>, <span data-dpFor="first_party_state_of_organization">${firstPartyState}</span> <span  data-dpFor="first_party_type">${firstPartyType}</span> (“<b>Discloser</b>”).`;
+  const renderData: string = `This Confidentiality Agreement (“<b>Agreement</b>”), by and between <span data-dpFor="first_party_name"></span>, <span data-dpFor="first_party_state_of_organization"></span> <span  data-dpFor="first_party_type"></span> (“<b>Discloser</b>”).`;
 
   return (
     <div>
